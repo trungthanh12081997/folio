229198e9373b1468f62b7c060cfe153496d33e68
TrungThanh12081997 <quantrungthanh73@gmail.com>
2024-12-26 10:12:40 +0700

    fix cicd

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -40,7 +40,7 @@ const Footer = () => {
           }}
           href="/Ayush_Resume.pdf"
         ></Button>
-        <Button
+        {/* <Button
           classes="ml-3"
           type={ButtonTypes.WHITE}
           name="Let's Talk"
@@ -49,7 +49,7 @@ const Footer = () => {
             target: "_blank",
             rel: "noreferrer",
           }}
-        ></Button>
+        ></Button> */}
       </div>
       <h2 className="text-center text-sm sm:text-base mt-8">
         Designed and Developed with ❤️ by Ayush
